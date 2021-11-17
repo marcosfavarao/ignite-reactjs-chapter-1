037471737c0d7560a2a1d7ab24826b4bb77a5d5b
Marcos Favarão <marcos.favarao@gmail.com>
2021-11-17 02:12:37 -0300

    Commit: Fetching API using useEffect() hook

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { RepositoryItem } from '../RepositoryItem';
 
 import './styles.scss';
 
+// https://api.github.com/users/MarcosFavarao/repos
+
 export const RepositoryList = () => {
+    const [repositories, setRepositories] = useState([]);
+
     const repository = {
         name: "Unform",
         description: "Forms in React",
         link: 'https://github.com/unform/unform',
     }
+
+    useEffect(() =>{
+        fetch('https://api.github.com/users/MarcosFavarao/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data));
+    }, []);
 
     return (
         <section className="repository-list">
@@ -20,4 +30,4 @@ export const RepositoryList = () => {
             </ul>
         </section>
     )
-}
+};
